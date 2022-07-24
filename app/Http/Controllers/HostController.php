da53d33c97deafd52f219a500a1765c742d24e5b
Fernando Clemente <fernandoclemente73@gmail.com>
2022-07-24 22:42:06 +0700

    update delete logic DONE

--- app/Http/Controllers/HostController.php
+++ app/Http/Controllers/HostController.php
@@ -74,17 +74,80 @@ class HostController extends Controller
 
     public function edit($id){
         $vehicle = Car::select('*')->find($id);
-        return view('updateVehicle',[
+        return view('dashboard.updateVehicle',[
             'title' => 'Update Vehicle' , 
             'active' => 'Admin' , 
             'vehicle' => $vehicle 
         ]);        
     }
 
-    public function destory($id){
+    public function update(Request $request,$id){
+
+        $vehicle = Car::find($id);
+
+        $rules = [
+            'carName' => 'required' , 
+            'plateNumber' => 'required', 
+            'price' => 'required' , 
+            'carImage' => 'mimes:png,jpg,jpeg,svg' , 
+            // userID
+            'carAddress' => 'required' , 
+            'status' => 'required' , 
+            'totalRating' => 'required' , 
+            'carType' => 'required' , 
+            'fuelType' => 'required' , 
+            'seat' => 'required' , 
+            'wheelModel' => 'required' , 
+            'bluetooth' => 'required' , 
+            'aux' => 'required' , 
+            'childSeat' => 'required' , 
+            'petFriendly' => 'required' , 
+            'noSmoking' => 'required' ,  
+        ] ; 
+
+        $request->validate($rules);
+
+        // Upload image
+        if( $request->carImage != null ){
+            $fileName = $request->carName.'.'.$request->carImage->extension();
+    
+            $request->file('carImage')->storeAs(
+                'public/carImage', $fileName
+            );
+        }else{
+            $fileName = $vehicle->carImage ;
+        }
+
+        /////
+
+        $vehicle->update([
+            'carName' => $request->carName,
+            'plateNumber' => $request->plateNumber,
+            'price' => $request->price,
+            'carImage' => $fileName,
+            'userID' => 1,
+            'carAddress' => $request->carAddress,
+            'status' => $request->status,
+            'totalRating' => $vehicle->totalRating,
+            'carType' => $request->carType,
+            'fuelType' => $request->fuelType,
+            'seat' => $request->seat,
+            'wheelModel' => $request->wheelModel,
+            'bluetooth' => $request->bluetooth,
+            'aux' => $request->aux,
+            'childSeat' => $request->childSeat,
+            'petFriendly' => $request->petFriendly,
+            'noSmoking' => $request->noSmoking,    
+        ]);
+
+        return redirect('/dashboard/carManager')->with('success', 'Game Updated Successfully!');
+
+    }
+
+
+    public function destroy($id){
         DB::delete('delete from cars where id = ?',[$id]);
-        return redirect('/manageGame')->with('success', 'Game Deleted Successfully!');
-        
+        return redirect('/dashboard/carManager')->with('success', 'Game Deleted Successfully!');
     }
 
 }
